33e44a24bc93203097caa8231f0c19a1ab5c937f
lucky-lalit <ltiwari2005@gmail.com>
2026-04-27 21:25:21 +0530

    done with week4 practice

---
+++ week4/pointer/pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *s = "HI!";
+    // printf("%c\n", *s);
+    // printf("%c\n", *(s+1));
+    // printf("%c\n", *(s+2));
+
+    printf("%s\n", s);
+    printf("%s\n", (s+1));
+    printf("%s\n", (s+2));
+    printf("%s\n", (s+3));
+}
+
